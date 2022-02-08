d6c85a7edda67a1d50512d16145119e320f8a604
jakkappro <jakkappro@gmail.com>
2022-02-08 12:33:28 +0100

    add query params to emailV component

--- src/app/Email/email-verification/email-verification.component.ts
+++ src/app/Email/email-verification/email-verification.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-email-verification',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailVerificationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.queryParams
+      .subscribe(params => {
+        console.log(params)
+        
+      });
   }
 
 }
